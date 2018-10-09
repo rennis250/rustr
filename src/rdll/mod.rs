2fbe8417e6101c8626baa90fa9c2f10a0d603cd7
Robert J. Ennis <rje@sdf.org>
2018-10-09 23:39:59 +0200

    Add preliminary support for OpenBSD

--- src/rdll/mod.rs
+++ src/rdll/mod.rs
@@ -4,14 +4,14 @@
 //!
 
 
-#[cfg(all(any(target_os="freebsd",target_os="linux",target_os="macos"), target_pointer_width = "64"))]
+#[cfg(all(any(target_os="freebsd",target_os="openbsd",target_os="linux",target_os="macos"), target_pointer_width = "64"))]
 pub mod unix64;
-#[cfg(all(any(target_os="freebsd",target_os="linux",target_os="macos"), target_pointer_width = "64"))]
+#[cfg(all(any(target_os="freebsd",target_os="openbsd",target_os="linux",target_os="macos"), target_pointer_width = "64"))]
 pub use self::unix64::*;
 
-#[cfg(all(any(target_os="freebsd",target_os="linux"), target_pointer_width = "32"))]
+#[cfg(all(any(target_os="freebsd",target_os="openbsd",target_os="linux"), target_pointer_width = "32"))]
 pub mod unix32;
-#[cfg(all(any(target_os="freebsd",target_os="linux"), target_pointer_width = "32"))]
+#[cfg(all(any(target_os="freebsd",target_os="openbsd",target_os="linux"), target_pointer_width = "32"))]
 pub use self::unix32::*;
 
 #[cfg(all(target_os="windows", target_pointer_width = "64"))]
